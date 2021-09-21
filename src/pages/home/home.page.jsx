5e54de60b807da1b070cefbf20821f8ee54b850d
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2021-09-21 12:46:02 +0200

    update

--- src/pages/home/home.page.jsx
+++ src/pages/home/home.page.jsx
@@ -1,12 +1,59 @@
 import React from 'react';
 import './style.home.css';
-import Hero from './hero';
+import img0 from '../../public/asserts/hero.JPG';
+import img1 from '../../public/asserts/home.jpg';
+import img2 from '../../public/asserts/profil.PNG'
+import Slider from 'react-slick';
+import { RightOutlined, LeftOutlined } from "@ant-design/icons";
 
+
+function SampleNextArrow(props) {
+    const { onClick } = props;
+    return (
+            <div className="div-scroll-rigth">
+                <button onClick={onClick}  className="btn-scroll-right"><RightOutlined /></button>
+            </div>
+    );
+  }
+  
+  function SamplePrevArrow(props) {
+    const { onClick } = props;
+    return (
+        <div className="div-scroll-left">
+        <button onClick={onClick}  className="btn-scroll-left"><LeftOutlined /></button>
+    </div>
+    );
+  }
 
 export default function Home(){
+    const settings = {
+        dots: false,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        slideToshow: 1,
+        slideToscroll: 1,
+        speed: 500,
+        nextArrow: <SampleNextArrow />,
+        prevArrow: <SamplePrevArrow />,
+        className: "div-caroussel"
+    }
+    const photos = [
+        img0, img1, img2,
+    ]
     return(
-        <div className="">
-            <Hero />
+        <div className="home-page">
+            <Slider {...settings}>
+                {
+                    photos.map(photo =>{
+                        return(
+                            <div data-aos="fade-up" className="div-img">
+                                <img className="slide" src={photo} alt="1"/>
+                            </div>
+                        );
+                    })
+                }
+            </Slider>
             <div className="container-fluid pb-2">
                 <h2 className="font-weight-bold text-white text-upperCase pb-2">Nos Options & Sections</h2>
             </div>
