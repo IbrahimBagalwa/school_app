ff59f433bd7ec2d623b5bfccef2ea7cd3a8e75e4
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2021-09-21 10:21:14 +0200

    nav component

---
+++ src/components/nav/index.jsx
@@ -0,0 +1,20 @@
+
+export default function Nav({children}){
+
+    return(
+
+        <div className="page">
+            <div className="nav">
+                <div className="nav-header">
+                    <Header />
+                </div>
+                <div className="content">
+                    {children}
+                </div>
+                <div className="nav-footer">
+                    <SideBarMob />
+                </div>
+            </div>
+        </div>
+    );
+}
