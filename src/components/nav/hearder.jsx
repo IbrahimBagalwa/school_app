90acf72c912738cd2e25f71d3843b179bd767deb
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2021-09-21 10:30:26 +0200

    navbar

---
+++ src/components/nav/hearder.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react'
+import {Link} from 'react-router-dom';
+import './navbar.component.css';
+
+function Header() {
+
+
+    return (<>
+        <nav class="navbar navbar-expand-lg navbar-dark fixed-top title">
+            <div className="container">
+                <Link className="navbar-brand fst-italic navbar-right mr-5" to="/">NDAHURA HIGTH SCHOOL</Link>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse justify-content-end float-left" id="navbarColor02">
+                    <ul className="navbar-nav">
+                       
+                            <li className="nav-item">
+                                <Link className="nav-link" to='/'>Accueil</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to='/'>Valve</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to='/'>Contact</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to='/'>Ndahura-Goma</Link>
+                            </li>
+                       
+                            <li className="nav-item">
+                                <Link to="#" className="nav-link">Créer un compte</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="#" className="nav-link">Se connecter</Link> 
+                            </li> 
+                           
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        </>
+    )
+}
+
+export default Header
