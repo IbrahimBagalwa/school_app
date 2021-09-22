f31a416c8d75ffd14d679e3c3ccdb2519ff2ac4d
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2021-09-22 10:12:20 +0200

    home page

---
+++ src/pages/valve/valve.page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Valve() {
+    return (
+        <div className="hero">
+            <div className="hero-info">
+                <h3 className="font-weight-bold text-white mb-0">NDAHURA-GOMA, Résultats scolaire</h3>
+            </div>
+        </div>
+    )
+}
