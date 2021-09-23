2710805e760076e60d22713b7b7bc97885193db2
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2021-09-23 13:11:46 +0200

    update

--- src/pages/valve/valve.page.jsx
+++ src/pages/valve/valve.page.jsx
@@ -1,84 +1,128 @@
 import React from 'react';
 import './style.valve.css';
-import { DataGrid } from '@material-ui/data-grid';
+import {DataGrid}  from '@material-ui/data-grid';
 import Avatar from '@material-ui/core/Avatar';
-import { Tag } from 'antd';
+// import { Tag } from 'antd';
 import InputBase from '@material-ui/core/InputBase';
 
 export default function Valve() {
    
 
-    const columns = [
-    { field: 'id', headerName: 'ID', width: 120 },
-    {
-        field: 'user',
-        headerName: 'User',
-        width: 280,
-        renderCell: (params)=>{
-            return(
-                <div className="userListUser">
-                    <Avatar className="avatar-userList" children={<img src={`${url}/resource/${params.row.avatar}`} alt={params.row.fsname} className="avatar"/>} />  {params.row.fsname} { params.row.lsname }
-                </div>
-            )
-        }
-    },
-    {
-        field: 'id_',
-        headerName: 'Matricule',
-        width: 180,
+    // const columns = [
+    // { field: 'id', headerName: 'ID', width: 120 },
+    // {
+    //     field: 'lastName',
+    //     headerName: 'User',
+    //     width: 280,
+    //     renderCell: (params)=>{
+    //         return(
+    //             <div className="userListUser">
+    //                 <Avatar className="avatar-userList" children={<img src={`${url}/resource/${params.row.avatar}`} alt={params.row.fsname} className="avatar"/>} />  {params.row.fsname} { params.row.lsname }
+    //             </div>
+    //         )
+    //     }
+    // },
+    // {
+    //     field: 'id_',
+    //     headerName: 'Matricule',
+    //     width: 180,
     
-    },
-    {
-        field: 'phone',
-        headerName: 'Phone',
-        width: 200,
+    // },
+    // {
+    //     field: 'phone',
+    //     headerName: 'Phone',
+    //     width: 200,
     
-    },
-    {
-        field: 'solde',
-        headerName: 'Solde',
-        width: 200,
-        renderCell: (params) =>{
-        return(
-            <div> {params.row.solde}FC </div>
-        )
-        }
-    },
-    {
-        field: 'role',
-        headerName: 'Role',
-        width: 200,
-        renderCell: (params) =>{
-        return(
-            <div> {params.row.role === 0 ? "Agent" : params.row.role === 1 ? "Admin" : params.row.role === 2 ? "Chauffeur" : "Others"}</div>
-        )
-        }
-        // editable: true,
-    },
-    {
-        field: 'datastatus',
-        headerName: 'Etat',
-        width: 180,
-        renderCell: (params)=>{
-            return (
-            <div>{ params.row.datastatus === 1 ? <Tag color='success'>Activé</Tag> : params.row.datastatus === 0 ? <Tag color='red'>Non Activé</Tag>  : null }</div>
+    // },
+    // {
+    //     field: 'solde',
+    //     headerName: 'Solde',
+    //     width: 200,
+    //     renderCell: (params) =>{
+    //     return(
+    //         <div> {params.row.solde}FC </div>
+    //     )
+    //     }
+    // },
+    // {
+    //     field: 'role',
+    //     headerName: 'Role',
+    //     width: 200,
+    //     renderCell: (params) =>{
+    //     return(
+    //         <div> {params.row.role === 0 ? "Agent" : params.row.role === 1 ? "Admin" : params.row.role === 2 ? "Chauffeur" : "Others"}</div>
+    //     )
+    //     }
+    //     // editable: true,
+    // },
+    // {
+    //     field: 'datastatus',
+    //     headerName: 'Etat',
+    //     width: 180,
+    //     renderCell: (params)=>{
+    //         return (
+    //         <div>{ params.row.datastatus === 1 ? <Tag color='success'>Activé</Tag> : params.row.datastatus === 0 ? <Tag color='red'>Non Activé</Tag>  : null }</div>
         
-            )
-        }
-    },
+    //         )
+    //     }
+    // },
     
-    ];
+    // ];
+    
+    
+    const columns = [
+        { field: 'id', headerName: 'ID', width: 120 },
+        {
+          field: 'action',
+          headerName: 'Action',
+          width: 150,
+        },
+        {
+          field: 'promotion',
+          headerName: 'Promotion',
+          width: 160,
+        },
+        {
+          field: 'session',
+          headerName: 'Session',
+          width: 220,
+          editable: true,
+        },
+        {
+          field: 'annee',
+          headerName: 'Année',
+          width: 160,
+        },
+        {
+            field: 'distinction',
+            headerName: 'Distinction',
+            width: 180,
+        },
+        {
+            field: 'satisfaction',
+            headerName: 'Satisfaction',
+            width: 180,
+        },
+        {
+            field: 'echoue',
+            headerName: 'Echoués',
+            width: 180,
+        },
+        {
+            field: 'datepublication',
+            headerName: 'Date',
+            width: 160,
+        },
+      ];
     const userData =[
         
-        { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-        { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-        { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-        { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-        { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-        { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-        { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-        { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+        { id: 1, action: 'Voir la grille' ,promotion: '6 H.T.S',session: 'Premiere session', annee:'2019-2020',distinction: '0 sur 20 (0,0%)', satisfaction: '15 sur 20 (85%)', echoue: '5 sur 20 (15%)', datepublication:'2019-2020' },
+        { id: 2, action: 'Voir la grille' ,promotion: '5 H.T.S',session: 'Premiere session', annee:'2019-2020',distinction: '0 sur 20 (0,0%)', satisfaction: '15 sur 20 (85%)', echoue: '5 sur 20 (15%)', datepublication:'2019-2020' },
+        { id: 3, action: 'Voir la grille' ,promotion: '4 H.T.S',session: 'Premiere session', annee:'2019-2020',distinction: '0 sur 20 (0,0%)', satisfaction: '15 sur 20 (85%)', echoue: '5 sur 20 (15%)', datepublication:'2019-2020' },
+        { id: 4, action: 'Voir la grille' ,promotion: '3 M.P',session: 'Premiere session', annee:'2019-2020',distinction: '2 sur 30 (0,0%)', satisfaction: '15 sur 20 (85%)', echoue: '5 sur 20 (15%)', datepublication:'2019-2020' },
+        { id: 5, action: 'Voir la grille' ,promotion: '5 H.P',session: 'Premiere session', annee:'2019-2020',distinction: '4 sur 20 (0,0%)', satisfaction: '15 sur 20 (85%)', echoue: '5 sur 20 (15%)', datepublication:'2019-2020' },
+        { id: 6, action: 'Voir la grille' ,promotion: '2 C.O',session: 'Premiere session', annee:'2019-2020',distinction: '5 sur 20 (0,0%)', satisfaction: '15 sur 20 (85%)', echoue: '5 sur 20 (15%)', datepublication:'2019-2020' },
+        { id: 7, action: 'Voir la grille' ,promotion: '1 C.O',session: 'Premiere session', annee:'2019-2020',distinction: '0 sur 20 (0,0%)', satisfaction: '15 sur 20 (85%)', echoue: '5 sur 20 (15%)', datepublication:'2019-2020' },
         
     ]
 
