5c47d4966120651a5ffbe98a6c943697dfd57b2a
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2021-09-23 14:28:17 +0200

    update

--- src/pages/register/register.page.jsx
+++ src/pages/register/register.page.jsx
@@ -4,47 +4,48 @@ import './style.register.css'
 
 function RegisterScreen(props) {
 
-         return  (
-             <div className="form">
-                 <form action="">
-                     <ul className="form-container">
-                         <li>
-                             <h2>Create Account</h2>
-                         </li>
-                         <li>
-                             <label htmlFor="name">
-                                 Name
-                             </label>
-                             <input type="name" name="name" id="name" />
-                         </li>
-                         <li>
-                             <label htmlFor="email">
-                                 Email
-                             </label>
-                             <input type="email" name="email" id="email"/>
-                         </li>
-                         <li>
-                             <label htmlFor="password">
-                                 Password
-                             </label>
-                             <input type="password" id="password" name="password"/>
-                         </li>
-                         <li>
-                             <label htmlFor="rePassword">
-                                 Confirm Password
-                             </label>
-                             <input type="password" id="rePassword" name="rePassword"/>
-                         </li>
-                         <li>
-                             <button type="submit" className="button primary">Envoyer</button>
-                         </li>
-                         <li>
-                             All ready have account? <Link to='/login'>Sign-in</Link>
-                         </li>
-                     </ul>
-                 </form>
-             </div>
-         )
+    return  (
+        <div className="form">
+            <form action="">
+                <ul className="form-container">
+                    <li>
+                        <h2>Demande d'inscription</h2>
+                    </li>
+                    <li>
+                        <label htmlFor="name">
+                            Nom
+                        </label>
+                        <input type="name" name="name" id="name" />
+                    </li>
+                    
+                    <li>
+                        <label htmlFor="email">
+                            Email
+                        </label>
+                        <input type="email" name="email" id="email"/>
+                    </li>
+                    <li>
+                        <label htmlFor="password">
+                            Password
+                        </label>
+                        <input type="password" id="password" name="password"/>
+                    </li>
+                    <li>
+                        <label htmlFor="rePassword">
+                            Confirm Password
+                        </label>
+                        <input type="password" id="rePassword" name="rePassword"/>
+                    </li>
+                    <li>
+                        <button type="submit" className="button primary">Envoyer</button>
+                    </li>
+                    <li>
+                        All ready have account? <Link to='/login'>Sign-in</Link>
+                    </li>
+                </ul>
+            </form>
+        </div>
+    )
 }
 
 export default RegisterScreen
