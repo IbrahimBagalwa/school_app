5746ffb93c0c1528aa1620ec7f4669af999b08b7
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2021-09-23 14:53:20 +0200

    update

--- src/pages/register/register.page.jsx
+++ src/pages/register/register.page.jsx
@@ -60,6 +60,10 @@ function RegisterScreen(props) {
                 </select>
             </div>
             <div className="newUserItem">
+               <label htmlFor="avatar">Copier du diplome de la classe Precedente</label>
+               <input className="avatarInput" type="file" placeholder="img.pjg..." accept="image/*"/>
+           </div>
+            <div className="newUserItem">
                 <label htmlFor="nomCompletTutaire">Nom Complet du tutaire</label>
                 <input type="text" placeholder="Peter Swedi Wasolela" />
             </div>
@@ -75,38 +79,7 @@ function RegisterScreen(props) {
                 <label htmlFor="">Address</label>
                 <input type="text" placeholder="Himbi | Goma" />
             </div>
-           
-            <div className="newUserItem">
-                <label>Role</label>
-                <select value={role} onChange={(e) =>{
-                    setRole(e.target.value);
-                    if(parseInt(e.target.value) === 2) setisconductor(true);
-                    else setisconductor(false); 
-                }
-                } className="newUserSelect" name="active" id="active">
-                    <option value="0">Agent</option>
-                    <option value="2">Chauffeur</option>
-                    <option value="3">Others</option>
-                </select>
-            </div>
-            {
-            isconductor ?
-            <>
-                <div className="newUserItem">
-                    <label>Taxis</label>
-                    <select value={idcategorie} onChange={(e) =>setCategory(e.target.value)} className="newUserSelect" name="active" id="active">
-                        <option value="1">Taxis Voiture</option>
-                        <option value="2">Taxis Bus</option>
-                        <option value="3">Taxis Moto</option>
-                    </select>
-                </div>
-                <div className="newUserItem">
-                    <label htmlFor="matricule">Matricule Taxis</label>
-                    <input type="text" placeholder="TY-9847-CHS1" onChange = {(e)=>{setMatriculeCar(e.target.value)}} />
-                </div> 
-            </>
-        : <div></div>
-        }   
+        
             <button type="submit" className="newUserButton">Create</button>
 
         </form>
