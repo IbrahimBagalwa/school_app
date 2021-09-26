69f205df8511a0807c582e19b3e18d205e1ff513
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2021-09-26 13:51:17 +0200

    update

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -2,10 +2,24 @@ import React from 'react'
 
 export default function About() {
     return (
-        <div className="hero">
-            <div className="hero-info">
-                <h3 className="font-weight-bold text-white text-center">NDAHURA-GOMA : A propos de nous</h3>
+        <>
+            <div className="hero">
+                <div className="hero-info">
+                    <h3 className="font-weight-bold text-white text-center">NDAHURA-GOMA : A propos de nous</h3>
+                </div>
             </div>
-        </div>
+            <div className="contaner">
+                <div className="row">
+                    <div className="col-md-12">
+                        <p><strong>I. Historique de l’INSTITUT NDAHURA </strong></p>
+                        <p>
+                            L’institut NDAHURA est une école conventionnée protestante de la communauté anglicane au Congo, créée en mémoire de l’Archevêque M. NDAHURA BEZALERE en 
+                            1981 ; c’est ainsi que cette institution portera son nom. Elle fut créée comme établissement privé, situé à l’époque dans la commune de KARISIMBI, avenue Bunia derrière le camp de policiers « MUNZENZE ». 
+                            Il a commencé à fonctionner au mois de septembre 1986 et comptait deux salles de classes   de cycle d’orientation, une de 1ère année et une autre de 2ème année et avait un effectif de 89 élèves. 
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
