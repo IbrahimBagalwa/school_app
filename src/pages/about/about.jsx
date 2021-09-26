7dfdb6207e1dfe56606c94495f11692188c18da6
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2021-09-26 14:00:30 +0200

    update

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -8,14 +8,29 @@ export default function About() {
                     <h3 className="font-weight-bold text-white text-center">NDAHURA-GOMA : A propos de nous</h3>
                 </div>
             </div>
-            <div className="contaner">
-                <div className="row">
+            <div className="container py-2">
+                <div className="row my-2 py-2">
                     <div className="col-md-12">
                         <p><strong>I. Historique de l’INSTITUT NDAHURA </strong></p>
-                        <p>
+                        <p className="ml-2">
                             L’institut NDAHURA est une école conventionnée protestante de la communauté anglicane au Congo, créée en mémoire de l’Archevêque M. NDAHURA BEZALERE en 
                             1981 ; c’est ainsi que cette institution portera son nom. Elle fut créée comme établissement privé, situé à l’époque dans la commune de KARISIMBI, avenue Bunia derrière le camp de policiers « MUNZENZE ». 
                             Il a commencé à fonctionner au mois de septembre 1986 et comptait deux salles de classes   de cycle d’orientation, une de 1ère année et une autre de 2ème année et avait un effectif de 89 élèves. 
+                        </p>
+                        <p className="ml-2">
+                            De 1987 à 1989, l’institut acquit trois nouvelles classes dont une classe de cycle 
+                            D’orientation et deux de troisième année commerciale et administrative, de 1988 à 1989, il eut cinq classes ; de 1989 à 1990, 
+                            il compta sept classe et il se sera agréé sous l’arrêté ministériel N0 DEP/CCE/001/0253/89 ; 
+                        </p>
+                        <p className="ml-2">
+                            De 1990 à 1991, l’institut compta huit classes comme école privée   du 14/10/1989 et aura atteint un effectif de 2566 élèves. 
+                            Vu la croissance de l’effectif, l’institué demeura gérant pour fonctionner au sein de la paroisse anglicane, sise sur avenue BUNIA n029, de 1991 à 1992, 
+                            l’institut acquiert en plus une nouvelle classe de la 6ème année CA. 
+                        </p>
+                        <p className="ml-2">
+                        De 1993 à nos jours, l’Institut NDAHURA a eu une nouvelle localisation, Dès 
+                        Lors, l’institut devint une école publique et transforma la section commerciale en section scientifique, option, mathématique physique, de 1993 à 1994, l’institut s’organise avec toutes les promotions proprement dites du cycle d’orientation, jus qu’en 6ème math-physique ; de 
+                        1995 à 1996, l’institut obtient une nouvelle section pédagogique sous l’arrêté numéro MINESP/CABIMIN/001/1768/85 du 25 octobre 1995, de 2004 à 2005 une autre section vit jour. 
                         </p>
                     </div>
                 </div>
