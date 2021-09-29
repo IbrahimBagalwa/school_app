737819d588f9fcda7fbc5f04700279fc7de3cfcd
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2021-09-29 17:16:12 +0200

    update

---
+++ src/redux/reducer/users/register.js
@@ -0,0 +1,33 @@
+import { USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS } from "../../constants/userConstant";
+
+export default (state, {type, payload}) =>{
+    switch (type) {
+        case USER_REGISTER_REQUEST:
+           return{
+               ...state,
+               register:{
+                    ...state.register,
+                    loading: true,
+                    error:null
+               }
+           };
+        case USER_REGISTER_SUCCESS:
+            return{
+                ...state,
+                register: {
+                    ...state.register,
+                    loading: false,
+                    data: payload,
+                }
+            };
+        case USER_REGISTER_FAIL:
+            return {
+                ...state,
+                register: {
+                    ...state.register,
+                    loading:false,
+                    error: payload
+                }
+            }
+    }
+}
