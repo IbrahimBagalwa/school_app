c471ea1d4ba874d3cfe07006cea7bcc685b625f0
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2021-09-29 17:17:27 +0200

    update

--- src/redux/reducer/users/index.js
+++ src/redux/reducer/users/index.js
@@ -1,7 +1,9 @@
 import initialStates from "../../initialStates";
 import login from "./login";
+import register from "./register";
 
 export default (state = initialStates.users, action = {})=>({
     ...state,
-    ...login(state, action)
+    ...login(state, action),
+    ...register(state, action)
 })
