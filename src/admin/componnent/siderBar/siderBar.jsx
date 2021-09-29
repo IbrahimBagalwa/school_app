b3b946e92711a3e597241c699f1e22c0e3496baf
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2021-09-30 01:05:30 +0200

    update

--- src/admin/componnent/siderBar/siderBar.jsx
+++ src/admin/componnent/siderBar/siderBar.jsx
@@ -69,24 +69,6 @@ export default function SideBar() {
                     </ul>
                 </div>
 
-                {/* <div className="sidebarMenu">
-                    <h3 className="sidebarTitle">Notifications</h3>
-                    <ul className="sidebarList">
-                       <li className="sidebarListItem">
-                            <MailOutline className="sidebarIcon"/>
-                            Mail   
-                        </li>
-                        <li className="sidebarListItem">
-                            <DynamicFeed className="sidebarIcon"/>
-                            Feedback   
-                        </li>
-                        <li className="sidebarListItem">
-                            <ChatBubbleOutline className="sidebarIcon"/>
-                            Messages   
-                        </li>   
-                    </ul>
-                </div> */}
-
                 <div className="sidebarMenu">
                     <h3 className="sidebarTitle">Corbeille</h3>
                     <ul className="sidebarList">
